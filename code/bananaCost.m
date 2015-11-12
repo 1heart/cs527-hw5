0336a138dcb28366e6d783596bf690817fd83f7a
1heart <yixin1996@gmail.com>
2015-11-11 20:57:22 -0500

    initial commit

---
+++ code/bananaCost.m
@@ -0,0 +1,15 @@
+function cost = bananaCost(a, b)
+
+cost.f = @banana;
+cost.OK = @OK;
+cost.a = a;
+cost.b = b;
+
+    function point = banana(x, cost, order)
+        % YOUR CODE HERE
+    end
+
+    function good = OK(~, ~, ~)
+        % YOUR CODE HERE
+    end
+end
