dd9b97fccd703010133c0e1dcb8873ea4ca14152
1heart <yixin1996@gmail.com>
2015-11-18 23:29:15 -0800

    pretty much done

--- 1b.m
+++ 1b.m
@@ -3,7 +3,8 @@ for i = 1:6
 	H = feature.current.H;
 	c = cond(H);
 	minS = min(svd(H));
-	disp(svd(H))
+	% disp(svd(H))
+	disp(H)
 	disp(strcat(num2str(i), ' & ', num2str(c, '%0.2f'), ' &  ', num2str(minS, '%0.2f'),  '\\'))
 end
 
